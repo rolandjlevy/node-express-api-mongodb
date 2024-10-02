78df26e66c72cbbc31617ee46d4703076548ab99
Roland Levy <contact@rolandlevy.co.uk>
2024-10-02 22:46:16 +0000

    implemented AUTHORIZED_ORIGINS with cors

--- api/index.js
+++ api/index.js
@@ -7,8 +7,27 @@ const router = require('../routes');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const authorizedOrigins = (process.env.AUTHORIZED_ORIGINS || '')
+  .split(',')
+  .map((origin) => origin.trim());
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || authorizedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders:
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use('/api', router);
-app.use(cors());
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, '..', 'public', 'index.html');
